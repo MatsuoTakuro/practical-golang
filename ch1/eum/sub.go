f52b2c9c998542c50c69b2ecbc85cbdc8011ac6d
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-05-09 10:41:54 +0900

    Move codes to enum pkg

---
+++ ch1/eum/sub.go
@@ -0,0 +1,59 @@
+package enum
+
+import "fmt"
+
+//go:generate enumer -type=CarType -json
+//go:generate enumer -type=CarOption -json
+
+type CarType int
+type CarOption uint64
+
+const (
+	Sedan CarType = iota + 1
+	Hatchback
+	MPV
+	SUV
+	Crossover
+	Coupe
+	Convertible
+)
+
+const (
+	GPS          CarOption = 1 << iota // 01(2)    = 1(10)
+	AWD                                // 10(2)    = 2(10)
+	SunRoof                            // 100(2)   = 4(10)
+	HeatedSeat                         // 1000(2)  = 8(10)
+	DriverAssist                       // 10000(2) = 16(10)
+)
+
+func Sub() {
+	carType()
+	carOption()
+}
+
+func carType() {
+	var t CarType
+	fmt.Println(t)
+	t = Sedan
+	fmt.Println(t)
+
+	carTypes := CarTypeValues()
+	for _, ct := range carTypes {
+		fmt.Println(ct)
+	}
+
+	t, _ = CarTypeString("MPV")
+	fmt.Println(t)
+
+	b, _ := t.MarshalJSON()
+	fmt.Println(string(b))
+}
+
+func carOption() {
+	var o CarOption
+	fmt.Println(o)
+	o = SunRoof | HeatedSeat // 100(2)  | 1000(2) = 1100(2) = 12(10)
+	if o&SunRoof != 0 {      // 1100(2) & 1000(2) =  100(2) = 4(10)
+		fmt.Printf("with %s\n", o)
+	}
+}
