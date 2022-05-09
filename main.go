5a25618fa4bf83afd14c27841fa19d952326281e
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-05-09 09:44:35 +0900

    1.3.5 use stringer cmd for outputting iota values as string

--- main.go
+++ main.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+//go:generate stringer -type=CarType
+//go:generate stringer -type=CarOption
+
+type CarType int
 type CarOption uint64
+
+const (
+	Sedan CarType = iota + 1
+	Hatchback
+	MPV
+	SUV
+	Crossover
+	Coupe
+	Convertible
+)
 
 const (
 	GPS          CarOption = 1 << iota // 01(2)    = 1(10)
@@ -13,9 +27,22 @@ const (
 )
 
 func main() {
-	o := SunRoof | HeatedSeat // 100(2)  | 1000(2) = 1100(2) = 12(10)
-	if o&SunRoof != 0 {       // 1100(2) & 1000(2) =  100(2) = 4(10)
-		fmt.Println("with SunRoof")
-	}
+	carType()
+	carOption()
+}
 
+func carType() {
+	var t CarType
+	fmt.Println(t)
+	t = Sedan
+	fmt.Println(t)
+}
+
+func carOption() {
+	var o CarOption
+	fmt.Println(o)
+	o = SunRoof | HeatedSeat // 100(2)  | 1000(2) = 1100(2) = 12(10)
+	if o&SunRoof != 0 {      // 1100(2) & 1000(2) =  100(2) = 4(10)
+		fmt.Printf("with %s\n", o)
+	}
 }
