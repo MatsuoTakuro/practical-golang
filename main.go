272c2a7dee94735c462c9f6663e844f7a7c7fd0b
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-05-09 09:10:32 +0900

    1.3.3 bit operation

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type CarOption uint64
+
+const (
+	GPS          CarOption = 1 << iota // 01(2)    = 1(10)
+	AWD                                // 10(2)    = 2(10)
+	SunRoof                            // 100(2)   = 4(10)
+	HeatedSeat                         // 1000(2)  = 8(10)
+	DriverAssist                       // 10000(2) = 16(10)
+)
+
 func main() {
-	fmt.Println("Hello, World.")
+	o := SunRoof | HeatedSeat // 100(2)  | 1000(2) = 1100(2) = 12(10)
+	if o&SunRoof != 0 {       // 1100(2) & 1000(2) =  100(2) = 4(10)
+		fmt.Println("with SunRoof")
+	}
+
 }
