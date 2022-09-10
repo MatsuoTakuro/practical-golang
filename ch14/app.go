66ddebf8b8b253d2f3d2eebeec9293206dc7700e
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-09-10 21:58:46 +0900

    14.2.1 build image with docker

---
+++ ch14/app.go
@@ -0,0 +1,7 @@
+package dockersample
+
+import "github.com/fatih/color"
+
+func AppMain() {
+	color.Blue("hello world\n")
+}
---
+++ ch14/cmd/sample/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "dockersample"
+
+func main() {
+	dockersample.AppMain()
+}
